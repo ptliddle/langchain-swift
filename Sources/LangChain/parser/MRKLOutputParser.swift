a128c12114c112a8110fc201ef742942827eb1f5
Peter Liddle <development.peter@liddle.network>
2024-01-01 17:47:51 -0600

    Change regex to handle multiline JSON response for ActionInput

--- Sources/LangChain/parser/MRKLOutputParser.swift
+++ Sources/LangChain/parser/MRKLOutputParser.swift
@@ -8,23 +8,23 @@
 import Foundation
 
 public struct MRKLOutputParser: BaseOutputParser {
+    
     public init() {}
+    
     public func parse(text: String) -> Parsed {
         print(text.uppercased())
         if text.uppercased().contains(FINAL_ANSWER_ACTION) {
             return Parsed.finish(AgentFinish(final: text))
         }
-        let pattern = "Action\\s*:[\\s]*(.*)[\\s]*Action\\s*Input\\s*:[\\s]*(.*)"
-        let regex = try! NSRegularExpression(pattern: pattern)
-        
+
+        // Adjusted pattern to capture multi-line JSON after "Action Input"
+        let pattern = "Action\\s*:\\s*(.*?)\\s*Action Input\\s*:\\s*(.*)"
+        let regex = try! NSRegularExpression(pattern: pattern, options: [.dotMatchesLineSeparators])
+
         if let match = regex.firstMatch(in: text, options: [], range: NSRange(location: 0, length: text.utf16.count)) {
-            
             let firstCaptureGroup = Range(match.range(at: 1), in: text).map { String(text[$0]) }
-//            print(firstCaptureGroup!)
-            
-            
             let secondCaptureGroup = Range(match.range(at: 2), in: text).map { String(text[$0]) }
-//            print(secondCaptureGroup!)
+
             return Parsed.action(AgentAction(action: firstCaptureGroup!, input: secondCaptureGroup!, log: text))
         } else {
             return Parsed.error
