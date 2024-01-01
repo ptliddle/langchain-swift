03368e0e8eb941af79d90eccc26a8550c4d8754d
Peter Liddle <development.peter@liddle.network>
2024-01-01 13:33:10 -0600

    Refactor Dalle to use base OpenAI class

--- Sources/LangChain/llms/Dalle.swift
+++ Sources/LangChain/llms/Dalle.swift
@@ -10,34 +10,26 @@ import NIOPosix
 import AsyncHTTPClient
 import OpenAIKit
 
-public class Dalle: LLM {
+public class Dalle: OpenAI {
+    
     let size: DalleImage.Size
-    public init(size: DalleImage.Size, callbacks: [BaseCallbackHandler] = [], cache: BaseCache? = nil) {
+    public init(apiKey: String, baseUrl: String, size: DalleImage.Size, callbacks: [BaseCallbackHandler] = [], cache: BaseCache? = nil) {
         self.size = size
-        super.init(callbacks: callbacks, cache: cache)
+        super.init(apiKey: apiKey, baseUrl: baseUrl, callbacks: callbacks, cache: cache)
     }
     
     public override func _send(text: String, stops: [String] = []) async throws -> LLMResult {
-        let env = Env.loadEnv()
         
-        if let apiKey = env["OPENAI_API_KEY"] {
-            let baseUrl = env["OPENAI_API_BASE"] ?? "api.openai.com"
-            let eventLoopGroup = ThreadManager.thread
-
-            let httpClient = HTTPClient(eventLoopGroupProvider: .shared(eventLoopGroup))
-            defer {
-                // it's important to shutdown the httpClient after all requests are done, even if one failed. See: https://github.com/swift-server/async-http-client
-                try? httpClient.syncShutdown()
-            }
-            let configuration = Configuration(apiKey: apiKey, api: API(scheme: .https, host: baseUrl))
-
-            let openAIClient = OpenAIKit.Client(httpClient: httpClient, configuration: configuration)
-            let reps = try await openAIClient.images.create(prompt: text, size: dalleTo(size: size))
-            return LLMResult(llm_output: reps.data.first!.url)
-        } else {
-            print("Please set openai api key.")
-            return LLMResult(llm_output: "Please set openai api key.")
+        let eventLoopGroup = ThreadManager.thread
+        let httpClient = HTTPClient(eventLoopGroupProvider: .shared(eventLoopGroup))
+        defer {
+            // it's important to shutdown the httpClient after all requests are done, even if one failed. See: https://github.com/swift-server/async-http-client
+            try? httpClient.syncShutdown()
         }
         
+        let openAIClient = try initiateChat(httpClient)
+        
+        let reps = try await openAIClient.images.create(prompt: text, size: dalleTo(size: size))
+        return LLMResult(llm_output: reps.data.first!.url)
     }
 }
